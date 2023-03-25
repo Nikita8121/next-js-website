4c2d99560cfac18a89918d43932603a8a2244c07
Nikita8121 <natanalegarh@gmail.com>
2023-03-25 19:17:40 +0300

    upd

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,7 +27,8 @@ export default class MyDocument extends Document {
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
-            rel="stylesheet"
+            rel="preload"
+            as="style"
           />
         </Head>
         <body>
